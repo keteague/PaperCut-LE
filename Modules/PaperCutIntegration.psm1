febf00550dd38689aa518a2ae2056ca5732beee8
keteague <kteague+github.com@pobox.com>
2025-09-11 16:06:08 -0500

    Update PaperCutIntegration.psm1

--- Modules/PaperCutIntegration.psm1
+++ Modules/PaperCutIntegration.psm1
@@ -1,44 +1,27 @@
-function Import-PfxToKeystore {
+function Import-PaperCutPfx {
     param(
         [string]$PfxPath,
-        [string]$KeytoolPath,
         [string]$KeystorePath,
-        [string]$PfxPassword,
-        [string]$KeystorePassword
+        [string]$KeytoolPath,
+        [string]$KeystorePassword,
+        [string]$PfxPassword
     )
-    if (Test-Path $KeystorePath) { Remove-Item $KeystorePath -Force }
+
+    if (Test-Path $KeystorePath) {
+        Remove-Item $KeystorePath -Force
+    }
 
     & $KeytoolPath -importkeystore `
-      -srckeystore  $PfxPath `
-      -srcstoretype pkcs12 `
-      -srcstorepass $PfxPassword `
-      -destkeystore $KeystorePath `
-      -deststorepass $KeystorePassword `
-      -destkeypass  $KeystorePassword `
-      -noprompt
+        -destkeystore $KeystorePath `
+        -deststorepass $KeystorePassword `
+        -srckeystore $PfxPath `
+        -srcstoretype PKCS12 `
+        -srcstorepass $PfxPassword `
+        -noprompt
 
-    if ($LASTEXITCODE -ne 0) { throw "Keytool import failed with exit code $LASTEXITCODE" }
-}
+    if ($LASTEXITCODE -ne 0) {
+        throw "Keytool import failed with exit code $LASTEXITCODE"
+    }
 
-function Update-ServerProperties {
-    param(
-        [string]$ServerPropsPath,
-        [string]$KeystorePath,
-        [string]$KeystorePassword
-    )
-    $ksPathForProps = ($KeystorePath -replace '\\','/')
-    $props = if (Test-Path $ServerPropsPath) { Get-Content $ServerPropsPath -Raw } else { "" }
-    $props = ($props -split "`r?`n" | Where-Object {
-        $_ -notmatch '^\s*server\.ssl\.keystore\s*=' -and
-        $_ -notmatch '^\s*server\.ssl\.keystore-password\s*=' -and
-        $_ -notmatch '^\s*server\.ssl\.key-password\s*='
-    }) -join "`r`n"
-    $props += "`r`nserver.ssl.keystore=$ksPathForProps"
-    $props += "`r`nserver.ssl.keystore-password=$KeystorePassword"
-    $props += "`r`nserver.ssl.key-password=$KeystorePassword`r`n"
-    Set-Content -Path $ServerPropsPath -Value $props -Encoding UTF8
-}
-
-function Restart-PaperCut {
-    Restart-Service -Name 'PCAppServer' -Force
+    Restart-Service PCAppServer -Force -ErrorAction Stop
 }
